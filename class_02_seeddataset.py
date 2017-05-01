d463c6cf2612d14e15d77a8aeb49fb0306901a8c
Swall0w <technext.jpn@gmail.com>
2017-05-02 01:26:19 +0900

    it plots true result of data

--- class_02_seeddataset.py
+++ class_02_seeddataset.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import pylab as pl
 
 def data_split(data,num):
     X = data[:,:num]
@@ -15,6 +17,12 @@ if __name__ == "__main__":
     data = np.loadtxt("seeds_dataset.txt",delimiter='\t')
     X,y = data_split(data,7)
 
+# Standardizing data
+    scaler = StandardScaler()
+    scaler.fit(X)
+    X_scaled = scaler.transform(X)
+    X = X_scaled
+
 # Clustering with K-means
     estimator = KMeans(n_clusters=3).fit(X)
 
@@ -24,7 +32,14 @@ if __name__ == "__main__":
 
 # Plot figre
     cmap = {0:'red',1:'blue',2:'green'}
-    plt.figure()
-    for (i,label) in enumerate(estimator.labels_):
-        plt.scatter(X_projection[i,0],X_projection[i,1],c=cmap[label])
+#    plt.figure()
+#    for (i,label) in enumerate(estimator.labels_):
+#        #plt.scatter(X_projection[i,0],X_projection[i,1],c=cmap[label])
+#        plt.text(X_projection[i,0],X_projection[i,1], str(y[i]), color=cmap[label])
+#    plt.show()
+    fig, ax = plt.subplots()
+#    ax.scatter(X_projection[:,0],X_projection[:,1])
+    for (i, label) in enumerate(estimator.labels_):
+        ax.scatter(X_projection[i,0],X_projection[i,1],c=cmap[label])
+        ax.annotate(str(int(y[i])),(X_projection[i,0],X_projection[i,1]),color=cmap[label])
     plt.show()
