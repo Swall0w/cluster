3ec0046c2fbb0734f9c7e1ca0c5756bf8c6d490c
Swall0w <technext.jpn@gmail.com>
2017-06-27 17:04:49 +0900

    [add]:Add plot function.

--- gmm_one_dimention.py
+++ gmm_one_dimention.py
@@ -25,7 +25,7 @@ def estimate_posterior_likelihood(X,pi, gf):
     l = np.zeros((X.size, pi.size))
     for (i, x) in enumerate(X):
         l[i,:] = gf(x)
-    return pi * l np.vectorize(lambda y: 1/y)(l.sum(axis = 1).reshape(-1,1))
+    return pi * l * np.vectorize(lambda y: 1/y)(l.sum(axis = 1).reshape(-1,1))
 
 def estimate_gmm_parameter(X, gamma):
     N = gamma.sum(axis=0)
@@ -33,7 +33,11 @@ def estimate_gmm_parameter(X, gamma):
     sigma = (gamma * (X.reshape(-1,1) - mu) ** 2).sum(axis=0)/N
     pi = N/X.size
     return (mu,sigma, pi)
-
+def calc_Q(X,mu, sigma, pi ,gamma):
+    Q = (gamma * (np.log(pi * (2 * np.pi * sigma) ** (-0.5)))).sum()
+    for (i, x) in enumerate(X):
+        Q += (gamma[i,:] * (-0.5 *  (x -mu)** 2 / sigma)).sum()
+    return Q
 def main():
     class_num = 2
     data_num = 1000 * class_num
@@ -54,6 +58,15 @@ def main():
         Q_new = calc_Q(X, mu, sigma, pi, gamma)
         delta = Q_new - Q
         Q = Q_new
+
+    print('mu* : {0} sigma* : {1}'.format(np.sort(np.around(mu_star,3)),np.sort(np.around(sigma_star,3))))
+    print('mu  : {0} sigma  : {1}'.format(np.sort(np.around(mu,3)),np.sort(np.around(sigma,3))))
+
+    n, bins,_ = pyplot.hist(X, 50, normed = 1, alpha = 0.3)
+    seq = np.arange(-15,15,0.02)
+    for i in range(class_num):
+        pyplot.plot(seq, gaussian(mu[i],sigma[i])(seq), linewidth= 2.0)
+    pyplot.show()
 
 
 
